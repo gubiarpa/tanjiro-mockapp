0e337fd4a48951d8e1fbdbaab6bfa41d415a3f59
Billy Arredondo <billy.arredondo@pucp.pe>
2021-10-08 03:41:55 -0500

    application sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,49 @@
 <template>
   <v-app>
-    <v-app-bar
+    <v-navigation-drawer
+      v-model="drawer"
       app
-      color="primary"
-      dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            {{ appName }}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Arpa-Soft
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-divider></v-divider>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-list
+        dense
+        nav
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>
+              {{ item.icon }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>{{ drawer ? '' : appName }}</v-toolbar-title>
+    </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>
@@ -44,12 +51,19 @@
 </template>
 
 <script>
-
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      appName: 'Printer CRM',
+      drawer: null,
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { title: 'Reminders', icon: 'mdi-bell-ring' },
+        { title: 'About', icon: 'mdi-help-box' },
+      ],
+      right: null,
+    }
+  },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
     name: 'Home',
 
     components: {
-      HelloWorld,
+      
     },
   }
 </script>
